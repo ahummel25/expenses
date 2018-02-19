7aac628f0610e59dd668dd80b35aa9e530e56208
Hummela <andrew.hummel@wwt.com>
2018-02-19 16:46:02 -0600

    Remove console.log

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -8,7 +8,6 @@ const ExpenseList = (props) => (
     <h1>Expense List</h1>
     {
       props.expenses.map((expense) => {
-        console.log({...expense});
          return <ExpenseListItem key={expense.id} {...expense}/>
       })
     }
