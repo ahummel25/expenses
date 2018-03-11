e5b0666571a74e5620057672807933e71f77a08b
Hummela <andrew.hummel@wwt.com>
2018-03-11 08:37:06 -0500

    Configured CSS extract and loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
-module.exports = {
-  entry: './src/app.js',
-  output: {
+module.exports = (env, argv) => {
+  const isProd = env === 'production';
+  const CSSExtract = new ExtractTextPlugin('styles.css');
+
+  return {
+    entry: './src/app.js',
+    output: {
       path: path.join(__dirname, 'public'),
       filename: 'bundle.js'
-  },
-  module: {
+    },
+    module: {
       rules: [
           {
             loader: 'babel-loader',
@@ -15,17 +20,32 @@ module.exports = {
           },
           {
             test: /\.s?css$/,
-            use: [
-              'style-loader',
-              'css-loader',
-              'sass-loader'
-            ]
+            use: CSSExtract.extract({
+              use: [
+                {
+                  loader: 'css-loader',
+                  options: {
+                    sourceMap: true
+                  }
+                },
+                {
+                  loader: 'sass-loader',
+                  options: {
+                    sourceMap: true
+                  }
+                }
+              ]
+            })
           }
       ]
-  },
-  devtool: 'cheap-module-eval-source-map',
-  devServer: {
-    contentBase: path.join(__dirname, "public"),
-    historyApiFallback: true
-  }
+    },
+    plugins: [
+      CSSExtract
+    ],
+    devtool: isProd ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: path.join(__dirname, "public"),
+      historyApiFallback: true
+    }
+  };
 };
