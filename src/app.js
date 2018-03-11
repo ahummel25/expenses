ff50929372a82a3556073e6a9a917dc7df6dbdc1
Hummela <andrew.hummel@wwt.com>
2018-03-11 07:48:43 -0500

    Remove dummy expense data

--- src/app.js
+++ src/app.js
@@ -12,14 +12,6 @@ import './styles/styles.scss';
 
 const store = configureStore();
 
-store.dispatch(addExpense({ description: 'Water Bill', amount: 5000, createdAt: 10001 }));
-store.dispatch(addExpense({ description: 'Gas bill', amount: 4000, createdAt: 10002 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 1950, createdAt: 10003 }));
-
-const state = store.getState();
-
-const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-
 const jsx = (
     <Provider store={store}>
       <AppRouter />
