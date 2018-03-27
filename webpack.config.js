ab1362c40754f5ed037ef6cfa7ef53014c07307e
Hummela <andrew.hummel@wwt.com>
2018-03-26 19:30:17 -0500

    Setup dev deps and dist folder

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = (env, argv) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -44,8 +44,9 @@ module.exports = (env, argv) => {
     ],
     devtool: isProd ? 'source-map' : 'inline-source-map',
     devServer: {
-      contentBase: path.join(__dirname, "public"),
-      historyApiFallback: true
+      contentBase: path.join(__dirname, 'public'),
+      historyApiFallback: true,
+      publicPath: '/dist/'
     }
   };
 };
